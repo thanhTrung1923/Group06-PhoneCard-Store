c13739aee1710358c1018a8641667147feacf8c8
huyanhluong123 <anhlhhe153263@fpt.edu.vn>
2025-12-16 05:23:31 +0700

    update order history

--- src/java/controller/OrderHistoryController.java
+++ src/java/controller/OrderHistoryController.java
@@ -8,36 +8,54 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.util.List;
 
 @WebServlet(name = "OrderHistoryController", urlPatterns = "/order-history")
 public class OrderHistoryController extends HttpServlet {
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginController</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
         HttpSession session = req.getSession();
+        
         User u = (User) session.getAttribute("account");
         if (u == null) {
-            resp.sendRedirect("login.jsp");
+            resp.sendRedirect("login");
             return;
         }
-         String status = req.getParameter("status");
+        String status = req.getParameter("status");
         String fromDate = req.getParameter("fromDate");
         String toDate = req.getParameter("toDate");
-        
+
         OrderDAO dao = new OrderDAO();
-       List<Order> orders = dao.filterOrders(
+        List<Order> orders = dao.filterOrders(
                 u.getUserId(),
                 status,
                 fromDate,
                 toDate
         );
-        
 
-        
-        
-   
         req.setAttribute("orders", orders);
         req.getRequestDispatcher("order-history.jsp").forward(req, resp);
     }
